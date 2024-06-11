bdd1783db0398020acb8816194d3342abddc2dc2
S-Marimbi <marimbinjunge@gmail.com>
2024-06-11 14:06:50 +0300

    SMP-12 Gameloop update

--- gameloop.py
+++ gameloop.py
@@ -1,6 +1,9 @@
 #intializing the game
 #initializing the board,tile bag, and players
-board = initialize_board()
-tile_bag = initialize_tile_bag()
-players = [Player('Human', tile_bag), Player('Computer', tile_bag)]
-current_player = 0
+# Function to start the game
+def start_game():
+    board = initialize_board()
+    tile_bag = initialize_tile_bag()
+    players = [Player('Human', tile_bag), Player('Computer', tile_bag)]
+    current_player = 0
+    return board, tile_bag, players, current_player
